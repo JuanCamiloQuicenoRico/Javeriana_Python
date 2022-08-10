887e552a21667113619f679266d9f44ed652491d
JuanCamiloQuicenoRico <110676109+JuanCamiloQuicenoRico@users.noreply.github.com>
2022-08-10 10:39:39 -0500

    Add files via upload

---
+++ 0x05-Functions.py.py
@@ -0,0 +1,20 @@
+#definir función
+def completsuma(sumando,sumado):
+     print("Resultado de la suma:",sumando+sumado)
+completsuma(3,8)
+print("--------------------")
+#definir función
+def completresta(minuendo,sustraendo):
+     print("Resultado de la resta:",minuendo-sustraendo)
+completresta(5,11)
+print("--------------------")
+#definir función
+def completmultiplicacion(multiplicando,multiplicador):
+     print("Resultado de la multiplicación:", multiplicando*multiplicador)
+completmultiplicacion(4,8)
+print("--------------------")
+#definir función
+def completpotenciacion (base,potenciacion):
+    print("Resultado de la potencia:", base**potenciacion)
+completpotenciacion(5,2)
+print("--------------------")
